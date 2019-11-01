1173fffb6844f077136fdd50d1a28bac36bacfcc
Kevin Pacheco <k.pacheco@me.com>
2019-11-01 13:30:07 -0700

    Update:     Added MSUnit Testing to project.
        Make sure to not push your App ID.

---
+++ MSTest_OpenWeatherMap/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenWeatherMapApi;
+using OpenWeatherMapApi.Core;
+
+namespace MSTest_OpenWeatherMap {
+	[TestClass]
+	public class UnitTest1 {
+		[TestMethod]
+		public void ValidInitialization() {
+			var config = new OpenWeatherMapConfiguration.Builder("").Build();
+
+			OpenWeatherMap.Initialize(config);
+
+			ICurrentWeather result =  OpenWeatherMap.GetCurrentWeather(35, 139);
+
+			Assert.IsNotNull(result);
+		}
+	}
+}
